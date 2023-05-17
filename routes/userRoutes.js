22a8cfcd3bddb8c1c33f6f14a5cc087e690d7fe9
Fhatuwani Raphalalani <fm.raphalalani29@gmail.com>
2023-05-17 23:08:31 +0200

    user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 
 router.get('/', UserController.findAll);
 router.get('/:id', UserController.findOne);
-router.post('/', UserController.create);
 router.put('/:id', UserController.update);
 router.delete('/:id', UserController.destroy);
 
